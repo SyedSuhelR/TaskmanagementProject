21e3c1d5cf6f32382aa49f11bc7381e8715f35c1
syed suhel R <syedsuhel1119@gmail.com>
2024-05-28 09:05:21 +0530

    Added Backend

--- backend/src/main/java/com/example/springboot/crud/operation/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot.crud.operation.repository;
-
-import com.example.springboot.crud.operation.model.TaskDetails;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<TaskDetails,Long> {
-}
